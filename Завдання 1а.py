f4c98e0102b61805f822c518620b3aa4d93a5008
polina0407 <147421205+polina0407@users.noreply.github.com>
2025-10-25 20:02:46 +0300

    Add files via upload

---
+++ Завдання 1а.py
@@ -0,0 +1,4 @@
+print("Завдання А: 2x - (4x - 5)(3 - 7x)")
+x = float(input("Введіть значення x: "))
+result_a = 2 * x - (4 * x - 5) * (3 - 7 * x)
+print("Результат A =", result_a)
---
+++ Завдання 2а.py
@@ -0,0 +1,6 @@
+print("\nЗавдання Б: (2x - 5) / (3 - 4y) - 7zy")
+x = float(input("Введіть значення x: "))
+y = float(input("Введіть значення y: "))
+z = float(input("Введіть значення z: "))
+result_b = (2 * x - 5) / (3 - 4 * y) - 7 * z * y
+print("Результат B =", result_b)
